048118ce972ba1fffaf53f90598b2ee955ae12da
Harrison Li <hli76@student.ubc.ca>
2025-12-03 15:50:45 -0800

    updated env for downloading data, finish download_data.py, create preprocessing script

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,16 +1,20 @@
+# download_data.py
+# author: Harrison Li
+# date: 2025-12-03
+
 import click
-import os
-import sys
-import requests
 import pandas as pd
-from pathlib import Path
 from ucimlrepo import fetch_ucirepo 
 
 @click.command()
-def main():
+@click.option('--pwd', type=str, 
+              help="Path to directory where raw data will be written to", default='~/data/raw')
+def main(pwd):
     """downloads data to data/raw"""
+    export_path = pwd + "/wine-raw.csv"
+    print(export_path)
     wine_raw = fetch_ucirepo(id=186).data.features
-    wine_raw.to_csv("../data/raw/wine-raw.csv")
+    wine_raw.to_csv(export_path)
     
 if __name__ == '__main__':
     main()
